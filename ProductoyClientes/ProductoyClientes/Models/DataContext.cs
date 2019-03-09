c555648ff8c2277d27578ad6a39eb092f11e56cf
Christian Alejandro Yabeta <christianyabeta@gmail.com>
2019-03-09 10:35:41 -0400

    Instalado el Entity Framework

---
+++ ProductoyClientes/ProductoyClientes/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductoyClientes.Models
+{
+    public class DataContext :DbContext
+    {
+    }
+}
---
+++ ProductoyClientes/ProductoyClientes/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductoyClientes.Models
+{
+    public class Producto
+    {
+
+        int ProdcutoID;
+        string Nombre;
+        double Precio;
+    }
+}
